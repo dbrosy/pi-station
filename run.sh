b2b03c11f6fb80371f1e9e01e5764cca57b97ec3
dbrosy <dbrosy@hotmail.com>
2017-01-30 00:24:31 +1100

    Update run.sh

--- run.sh
+++ run.sh
@@ -4,4 +4,4 @@
 # http://www.raspberrypi-spy.co.uk/2014/05/how-to-autostart-apps-in-rasbian-lxde-desktop/
 
 echo "Starting Weather Station"
-/usr/bin/python /home/pi/pistation/pistaton.py
+/usr/bin/python /home/pi/pi-station/pistaton.py
